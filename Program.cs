bcf6a2a06b9499981685e7efff6158be45c01fd8
Saurabh Jagdish Prasad Verma <vermas12@usf.edu>
2024-02-28 10:49:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int[] selection_sort_num = { 5, 72, 3, 14, 7 ,1};
+
+Console.WriteLine("The array before executing the Selection Sort: ");
+for (int i = 0; i < selection_sort_num.Length; i++)
+{
+    Console.Write("  " + selection_sort_num[i]);
+}
+Console.WriteLine("");
+Console.WriteLine("The array after executing the Selection Sort: ");
+selection_sort(selection_sort_num);
+Console.WriteLine("");
+
+
+static void selection_sort(int[] ss_arr)
+{
+    int min_pos;
+    int temp;
+
+    for (int i = 0; i < ss_arr.Length; i++)
+    {
+        min_pos = i;
+        for (int x = i + 1; x < ss_arr.Length; x++)
+        {
+            if (ss_arr[x] < ss_arr[min_pos])
+            {
+                min_pos = x;
+            }
+        } 
+        if (min_pos != i)
+        {
+            temp = ss_arr[i];
+            ss_arr[i] = ss_arr[min_pos];
+            ss_arr[min_pos] = temp;
+        }
+        Console.Write("  " + ss_arr[i]);
+    } 
+}
